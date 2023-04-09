6c2f73927a150a7e4d594be90506e4a69c6fe85f
Lê Quang Doanh <82566414+lequangdoanh@users.noreply.github.com>
2023-04-09 23:27:39 +0700

    Add files via upload

---
+++ client/public/data/products.js
@@ -0,0 +1,116 @@
+import room4 from '../images/room4.jpg'
+import room1 from '../images/room1.jpg'
+import room2 from '../images/room2.jpg'
+
+const products = [
+    {
+      id: "01",
+      productName: "Stone and Beam Westview ",
+      imgUrl: room4,
+      category: "Single Room",
+      price: 193,
+      shortDesc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+      reviews: [
+        {
+          rating: 4.7,
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+      avgRating: 4.5,
+    },
+    {
+        id: "02",
+        productName: "Stone and Beam Westview ",
+        imgUrl: room1,
+        category: "Single Room",
+        price: 193,
+        shortDesc:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+        reviews: [
+          {
+            rating: 4.7,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          },
+        ],
+        avgRating: 4.5,
+      },
+      {
+        id: "03",
+        productName: "Stone and Beam Westview ",
+        imgUrl: room2,
+        category: "Single Room",
+        price: 193,
+        shortDesc:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+        reviews: [
+          {
+            rating: 4.7,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          },
+        ],
+        avgRating: 4.5,
+      },
+      {
+        id: "01",
+        productName: "Stone and Beam Westview ",
+        imgUrl: room4,
+        category: "Double Room",
+        price: 193,
+        shortDesc:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+        reviews: [
+          {
+            rating: 4.7,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          },
+        ],
+        avgRating: 4.5,
+      },
+      {
+          id: "02",
+          productName: "Stone and Beam Westview ",
+          imgUrl: room1,
+          category: "Double Room",
+          price: 193,
+          shortDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+          reviews: [
+            {
+              rating: 4.7,
+              text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            },
+          ],
+          avgRating: 4.5,
+        },
+        {
+          id: "03",
+          productName: "Stone and Beam Westview ",
+          imgUrl: room2,
+          category: "Double Room",
+          price: 193,
+          shortDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iure quas illo voluptates labore tempore!",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nostrum accusantium iste, voluptas cumque provident! Consequatur officiis animi rem tempore voluptate cumque hic similique aperiam ut consectetur distinctio repudiandae quia quam quos, quas illo, iusto, necessitatibus odio veniam exercitationem quis voluptatibus debitis laboriosam! Esse debitis obcaecati blanditiis at impedit quibusdam!",
+          reviews: [
+            {
+              rating: 4.7,
+              text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            },
+          ],
+          avgRating: 4.5,
+        },
+];
+
+export default products;
