515d88b7c9ff369d35758e1ccad8ab221ff7471d
Lê Quang Doanh <82566414+lequangdoanh@users.noreply.github.com>
2023-05-17 18:52:04 +0700

    update api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,38 +1,9 @@
-const router = require("express").Router();
-const passport = require("passport");
+import express from "express";
+import { login, register } from "../controllers/auth.js";
 
-router.get("/login/success", (req, res) => {
-	if (req.user) {
-		res.status(200).json({
-			error: false,
-			message: "Successfully Loged In",
-			user: req.user,
-		});
-	} else {
-		res.status(403).json({ error: true, message: "Not Authorized" });
-	}
-});
+const router = express.Router();
 
-router.get("/login/failed", (req, res) => {
-	res.status(401).json({
-		error: true,
-		message: "Log in failure",
-	});
-});
+router.post("/register", register)
+router.post("/login", login)
 
-router.get("/google", passport.authenticate("google", ["profile", "email"]));
-
-router.get(
-	"/google/callback",
-	passport.authenticate("google", {
-		successRedirect: process.env.CLIENT_URL,
-		failureRedirect: "/login/failed",
-	})
-);
-
-router.get("/logout", (req, res) => {
-	req.logout();
-	res.redirect(process.env.CLIENT_URL);
-});
-
-module.exports = router;
+export default router
