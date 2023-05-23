5f315258534d4f6f9d85271864c2dd3a5ef7658b
nthinhh <104981351+nthinhh@users.noreply.github.com>
2023-05-23 14:13:10 +0700

    Update Featured

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -19,7 +19,7 @@ const Featured = () => {
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1>Berlin</h1>
+              <h1>Đà Nẵng</h1>
               <h2>{data[0]} properties</h2>
             </div>
           </div>
@@ -31,7 +31,7 @@ const Featured = () => {
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1>Madrid</h1>
+              <h1>Sài Gòn</h1>
               <h2>{data[1]} properties</h2>
             </div>
           </div>
@@ -42,7 +42,7 @@ const Featured = () => {
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1>London</h1>
+              <h1>Hà Nội</h1>
               <h2>{data[2]} properties</h2>
             </div>
           </div>
@@ -53,3 +53,4 @@ const Featured = () => {
 };
 
 export default Featured;
+
